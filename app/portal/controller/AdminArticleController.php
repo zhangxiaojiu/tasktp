08c60f6d3b34273b2c3d6fa4aa2c1648fd4c1438
Mac.xiaojiu <274276153@qq.com>
2018-05-04 21:53:26 +0800

    9-better-join-task-list

--- app/portal/controller/AdminArticleController.php
+++ app/portal/controller/AdminArticleController.php
@@ -65,6 +65,11 @@ class AdminArticleController extends AdminBaseController
     public function join(){
         $param = $this->request->param();
         $list = JoinPostService::JoinPostList($param);
+
+        $this->assign('start_time', isset($param['start_time']) ? $param['start_time'] : '');
+        $this->assign('end_time', isset($param['end_time']) ? $param['end_time'] : '');
+        $this->assign('keyword', isset($param['keyword']) ? $param['keyword'] : '');
+        $this->assign('join_status', isset($param['join_status']) ? $param['join_status'] : 1);
         $this->assign('list',$list);
         return $this->fetch();
     }
